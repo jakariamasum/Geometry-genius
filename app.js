646af57d707d485fb9f42f3098c2666f4fc49d8e
jakariamasum <jmphy698@gmail.com>
2023-02-18 12:19:20 +0600

    rectangle functionality added

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ document.getElementById('triangle').addEventListener('click',function(){
     const area=0.5*base*height; 
     displayData(serialNumber,name,area)
 }); 
+document.getElementById('rectangle').addEventListener('click',function(){
+    serialNumber++;
+    const name=getTextElementById('rectangle-name')
+    const base=getInputValue('rectangle-width');
+    const height=getInputValue('rectangle-length'); 
+    const area=base*height; 
+    displayData(serialNumber,name,area)
+}); 
 
 
 document.getElementById('Parallelogram').addEventListener('click',function(){
